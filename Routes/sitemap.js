d45c2d6ed8d2eb65dcd8bf05dc7c4e7e142e3bab
jozzydgr8 <jozzydgreat1@gmail.com>
2025-11-22 12:26:23 -0800

    updated sitemap

--- Routes/sitemap.js
+++ Routes/sitemap.js
@@ -1,14 +1,17 @@
-
 const router = require('express').Router();
-const Blog = require('../Schema/blogSchema'); // adjust path as needed
+const Blog = require('../Schema/blogSchema'); 
 
 router.get('/sitemap.xml', async (req, res) => {
     try {
-        const baseUrl = 'https://jozzycodes.com'; // Replace with your domain
+        const baseUrl = 'https://jozzycodes.com';
+
         const staticPaths = [
             '/',
             '/pricing',
             '/portfolio',
+            "/website-developer-in-lagos",
+            "/website-developer-in-nigeria",
+            "/affordable-website-design-lagos",
             '/blog',
             '/gb',
             '/gb/pricing',
@@ -16,23 +19,42 @@ router.get('/sitemap.xml', async (req, res) => {
             '/gb/blog',
         ];
 
-        // Fetch blog slugs from MongoDB
-        const blogs = await Blog.find({}, 'slug'); // Only fetch the slug field
-        const blogPaths = blogs.map(blog => `/blog/${blog.slug}`);
-        const gbBlogPaths = blogs.map(blog => `/gb/blog/${blog.slug}`);
+        // Fetch blog slugs
+        const blogs = await Blog.find({}, 'slug updatedAt');
 
-        const allPaths = [...staticPaths, ...blogPaths, ...gbBlogPaths];
+        const blogPaths = blogs.map(blog => ({
+            path: `/blog/${blog.slug}`,
+            lastmod: blog.updatedAt || new Date()
+        }));
 
+        const gbBlogPaths = blogs.map(blog => ({
+            path: `/gb/blog/${blog.slug}`,
+            lastmod: blog.updatedAt || new Date()
+        }));
+
+        const staticUrlObjects = staticPaths.map(path => ({
+            path,
+            lastmod: new Date()
+        }));
+
+        const allPaths = [...staticUrlObjects, ...blogPaths, ...gbBlogPaths];
+
+        // Build XML
         const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-${allPaths.map(path => `
+<urlset 
+  xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+>
+${allPaths.map(item => `
   <url>
-    <loc>${baseUrl}${path}</loc>
-  </url>`).join('\n')}
+    <loc>${baseUrl}${item.path}</loc>
+    <lastmod>${new Date(item.lastmod).toISOString()}</lastmod>
+  </url>
+`).join('\n')}
 </urlset>`;
 
         res.header('Content-Type', 'application/xml');
         res.send(sitemap);
+
     } catch (err) {
         console.error('Error generating sitemap:', err);
         res.status(500).send('Error generating sitemap');
